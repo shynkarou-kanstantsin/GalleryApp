30d9a0fe5da62cc4bca24fdc10e99cab7b172370
shynkarou-kanstantsin <shynkarou.kanstantsin@gmail.com>
2025-12-04 13:31:38 +0300

    Добавьте файлы проекта.

---
+++ GalleryApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "GalleryApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "GalleryApp.Pages")]
