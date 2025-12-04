19543aa593dbaf681f1176d82f88afa2b6e0566b
shynkarou-kanstantsin <shynkarou.kanstantsin@gmail.com>
2025-12-04 16:41:23 +0300

    Added PhotoModel

---
+++ GalleryApp/Models/PhotoModel.cs
@@ -0,0 +1,13 @@
+﻿namespace GalleryApp.Models
+{
+    public class PhotoModel
+    {
+        public string Id { get; set; }
+        public string SmallUrl { get; set; }
+        public string RegularUrl { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorLink { get; set; }
+        public string Description { get; set; }
+        public bool IsFavourite { get; set; }
+    }
+}
